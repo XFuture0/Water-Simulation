cff0d3c5fc764183275f32a3003cbba40c2537be
XFuture <3480487849@qq.com>
2026-05-15 14:16:47 +0800

    feat: 添加水阻力和水角阻力

--- Assets/Scripts/Item/Boat/Buoyant_Object.cs
+++ Assets/Scripts/Item/Boat/Buoyant_Object.cs
@@ -3,23 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody))]
 public class Buoyant_Object : MonoBehaviour
 {
-    private Rigidbody rb;
+    public Rigidbody rb;
     public Transform SeaFace;
     public float depthforceoffect = 3;
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    public float BuoyantPointCount = 1;
+    public float WaterDrag;
+    public float WaterAngular;
     private void Update()
     {
+        rb.AddForceAtPosition(Physics.gravity / BuoyantPointCount, transform.position, ForceMode.Acceleration);
         float WaveHeight = SeaFace.position.y + WaveManager.Instance.GetWaveHeight(transform.position.x);
         if(transform.position.y < WaveHeight)
         {
             float depthforce = Math.Clamp(WaveHeight - transform.position.y, 0, 1) * depthforceoffect;
-            rb.AddForce(Vector3.up * depthforce, ForceMode.Acceleration);
+            rb.AddForceAtPosition(new Vector3(0,depthforce, 0), transform.position, ForceMode.Acceleration);
+            rb.AddForce(-rb.velocity * depthforce * WaterDrag * Time.deltaTime,ForceMode.VelocityChange);
+            rb.AddTorque(-rb.angularVelocity * depthforce * WaterAngular * Time.deltaTime,ForceMode.VelocityChange);
         }
     }
 }
